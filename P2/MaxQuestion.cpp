248c393f29ed78739803b4016aa2635839daf681
Jynxae <realkhalily@hotmail.com>
2023-08-28 00:17:02 -0500

    adjusted maxProfit function

--- P2/MaxQuestion.cpp
+++ P2/MaxQuestion.cpp
@@ -5,35 +5,32 @@ using namespace std;
 
 int maxProfit(vector<int>& prices)
     {
-        int price = 0;
+        int maxdiff = 0;
         for (int x = 0; x < prices.size(); x++) // goes through each day
         {
             for (int y = x; y < prices.size(); y++) // for each day, it goes through the price
             {
-                int total = prices.at(y) - prices.at(x);
-                if (total > price)
-                {
-                    price = total;
-                }
+                maxdiff = max(maxdiff, prices.at(y) - prices.at(x));
             }
         }
-        return price;
+        return maxdiff;
     }
 
 int main()
 {
+    ifstream fin("input1.txt");
     int size = 0;
     int num = 0;
-    cout << "Reem is here " << endl;
-    cin >> size;
-    cout << size+1 << endl;
-    // cin.ignore();
+    fin >> size;
+    fin.ignore();
     vector<int> input;
-    // for(int x = 0; x < size; x++)
-    // {
-    //     cin >> num;
-    //     input.push_back(num);
-    // }
+    for(int x = 0; x < size; x++)
+    {
+        fin >> num;
+        input.push_back(num);
+    }
+
+    cout << maxProfit(input);
 
     return 0;
 }
