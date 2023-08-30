bbdfca10de84f97c40d75b47b415c70066fee927
Jynxae <realkhalily@hotmail.com>
2023-08-30 17:56:18 -0500

    Update ChessWinsAndLosses.py
    Adjusted if conditions. Found out how to count in a certain range of the list. Replaced split with the index only.

--- P14/ChessWinsAndLosses.py
+++ P14/ChessWinsAndLosses.py
@@ -1,12 +1,11 @@
-
 from typing import List
 
-def chess(chess, cond):
+def chessFun(chess, cond):
     if len(chess) <= 3:
         return
     else:
         half = len(chess)//2
-        chess = chess.split(half)
+        chess = chess[half:]  #splice is how we can use it to cute a list in half. split() will make each character its own
         chess(chess,True)
     
     Wcount = chess.count('W') #We make its own count cause we wanna check if there has been any W by itself in the last 4 games
@@ -17,13 +16,13 @@ def chess(chess, cond):
 
     #Checck to see in the last 3 games if there has been more L than W+D. ONLY LAST 3 GAMES!!!!!!!!!!
     #IF         L                     IN THE LAST 3 SPOTS        IS GREATER THAN             W+D                      IN THE LAST 3 SPOTS
-    if chess.count('L') in chess[-3:] > chess.count('W') + chess.count('D') in chess[-3:]:
+    if chess[-3:].count('L') > chess[-3:].count('W') + chess[-3:].count('D'):
        cond = False
        return
 
 
     #Check to see if there any W in the last 4 games
-    if chess.count('W') == 0 in chess[-4:]:
+    if chess[-4:].count('W'):
         cond = False
         return
     
@@ -32,7 +31,7 @@ def chess(chess, cond):
     return
 
 ChessWinsandLosses = ['W','L','L','L']
-result = chess(ChessWinsandLosses, True)
+result = chessFun(ChessWinsandLosses, True)
 
 if result == False:
     print("False")
