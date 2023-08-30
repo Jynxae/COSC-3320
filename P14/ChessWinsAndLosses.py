f48dfc6ed419679076e1e2188231750b30e1c074
Jynxae <realkhalily@hotmail.com>
2023-08-30 17:15:39 -0500

    Update ChessWinsAndLosses.py
    Adjusted if conditions and added main function. First completed code

--- P14/ChessWinsAndLosses.py
+++ P14/ChessWinsAndLosses.py
@@ -1,30 +1,40 @@
 
 from typing import List
 
-ChessWinsandLosses = ['W','L','L','L']
-
 def chess(chess, cond):
-    if len(ChessWinsandLosses) <= 3:
+    if len(chess) <= 3:
         return
     else:
-        half = len(ChessWinsandLosses)//2
-        ChessWinsandLosses = ChessWinsandLosses.split(half)
-        chess(ChessWinsandLosses,True)
+        half = len(chess)//2
+        chess = chess.split(half)
+        chess(chess,True)
     
-    Lcount = ChessWinsandLosses.count('L')
-    Wcount = ChessWinsandLosses.count('W') #We make its own count cause we wanna check if there has been any W by itself in the last 4 games
-    Dcount = ChessWinsandLosses.count('D')
-    WDcount = ChessWinsandLosses.count('D') + ChessWinsandLosses.count('W')
+    Wcount = chess.count('W') #We make its own count cause we wanna check if there has been any W by itself in the last 4 games
 
     if Wcount == 0:
         cond = False
+        return
 
-    #Checck to see in the last 3 games if there has been more L than W+D
-        #cond = False
+    #Checck to see in the last 3 games if there has been more L than W+D. ONLY LAST 3 GAMES!!!!!!!!!!
+    #IF         L                     IN THE LAST 3 SPOTS        IS GREATER THAN             W+D                      IN THE LAST 3 SPOTS
+    if chess.count('L') in chess[-3:] > chess.count('W') + chess.count('D') in chess[-3:]:
+       cond = False
+       return
 
 
     #Check to see if there any W in the last 4 games
-        #cond = False
+    if chess.count('W') == 0 in chess[-4:]:
+        cond = False
+        return
     
-    #if all passes then we do
-    #cond = True
+    #If we win all the games, then we keep the game condition true
+    cond = True
+    return
+
+ChessWinsandLosses = ['W','L','L','L']
+result = chess(ChessWinsandLosses, True)
+
+if result == False:
+    print("False")
+else:
+    print("True")
