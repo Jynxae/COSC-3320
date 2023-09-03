5b7ca7486587a316e1ea5f6ea1d4ce49b85584db
Jynxae <realkhalily@hotmail.com>
2023-09-03 00:44:43 -0500

    Divide into K sets
    Started the majority of the code, currently troubleshooting with what to do about the nextNum since its a local variable

--- P13/DistanceinBST.cpp
+++ P13/DistanceinBST.cpp
@@ -3,7 +3,23 @@
 #include<fstream>
 using namespace std;
 
+struct node{
+    int value;
+    node* right;
+    node* left;
+    node* next;
+};
+
+void insert(int val)
+{
+    node* temp = nullptr;
+    temp->value = val;
+    temp->next = nullptr;
+
+    
+}
+
 int main(){
 
-
+return 0;
 }
