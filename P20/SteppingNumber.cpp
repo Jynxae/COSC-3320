e3fceb8b88482d156343a822eaac1c4e75354981
Jynxae <realkhalily@hotmail.com>
2023-09-30 02:05:58 -0500

    Update almost correct p20
    PASSING ALMOST ALL TEST CASES!!!
    figuring out whats wrong with test case 7

--- P20/SteppingNumber.cpp
+++ P20/SteppingNumber.cpp
@@ -6,28 +6,47 @@ void step(int low, int high)
 {
     if(high <= low)
     {
-        cout << high << " ";
+        if (high <= 9)
+        {
+            cout << high << " ";
+        }
+        return;
     }
     else if (high <= 9)
     {
-        cout << high << " ";
         step(low, high - 1);
+        cout << high << " ";
     }
     else
     {
         int lastDigit = high % 10; // grab the last digit
-        int divDigit = high / 10;
-        if (abs(lastDigit - divDigit) == 1 || abs(divDigit - lastDigit == 1))
+        int divDigit = high / 10; //everything left side of the digit
+        bool stepping = true;
+        while(divDigit > 9)
+        {
+            if(abs((divDigit % 10) - lastDigit) != 1 || abs(lastDigit - (divDigit % 10)) != 1)
+            {
+                stepping = false;
+                break;
+            }
+            lastDigit = divDigit % 10;
+            divDigit /= 10;
+        }
+        
+        if ((abs(lastDigit - divDigit) == 1 || abs(divDigit - lastDigit == 1)) && stepping == true)
         {
             // recurse
             step(low, high - 1);
             cout<< high << " ";
         }
+        else{
+            step(low, high - 1);
+        }
     }
 }
 
 int main(){
-    ifstream fin("input1.tx3t");
+    ifstream fin("input1.txt");
     int low, high;
     fin >> low;
     fin.ignore();
